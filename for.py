8c3de4133e10dcca5238bdb64c0f0c3861fbbecf
LNMIG <migliorenicolas@yahoo.com.ar>
2022-08-19 21:31:13 -0300

    DOCS: cicles for and while

---
+++ for.py
@@ -0,0 +1,8 @@
+def run():
+    #for counter in range(1001): el contador va de 0 a 1000
+    for counter in range(1, 1001): #el contador va de 1 a 1000
+        print(counter)
+
+
+if __name__ == '__main__':
+    run()
